6d77a3ac59e8719e36e7be5318571ed56e702911
romelmain <romelherreratgk@gmail.com>
2025-02-27 22:35:26 -0400

    Creating Main Menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,23 @@
 import { Component } from '@angular/core';
-import { Router, RouterLink, RouterOutlet } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent, RouterLink],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'blue_oasis_app';
+  path: Array<String>;
   constructor(private router: Router) {
     if (localStorage.getItem('token')) {
       console.log('Hay Token');
-      this.router.navigate(['']);
+      this.path = [''];
     } else {
       console.log('No hay Token');
-      this.router.navigate(['/login']);
+      this.path = ['/login'];
     }
+    this.router.navigate(this.path);
   }
 }
