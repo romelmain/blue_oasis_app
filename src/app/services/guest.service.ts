a9b4808a16a19ce0d0c919e3b2a0c8fd5258feb3
romelmain <romelherreratgk@gmail.com>
2025-03-17 23:15:19 -0400

    Create new guest user form

--- src/app/services/guest.service.ts
+++ src/app/services/guest.service.ts
@@ -6,18 +6,21 @@ import Guest from '../models/Guest';
   providedIn: 'root',
 })
 export class GuestService {
-  readonly API_URL = 'http://localhost:8080/guest/';
+  readonly API_URL = 'http://localhost:8080/guest';
   token = localStorage.getItem('token');
   authorization: string = 'Bearer ' + this.token;
   constructor(private http: HttpClient) {}
 
   getGuestByUser(user: string) {
     const headers = { Authorization: this.authorization };
-    return this.http.get<any>(`${this.API_URL}user/${user}`, { headers });
+    return this.http.get<any>(`${this.API_URL}/user/${user}`, { headers });
   }
 
   postGuest(guest: Guest) {
-    const headers = { Authorization: this.authorization };
+    const headers = {
+      Authorization: this.authorization,
+      'Content-Type': 'application/json',
+    };
     return this.http.post<any>(this.API_URL, guest, { headers });
   }
 }
