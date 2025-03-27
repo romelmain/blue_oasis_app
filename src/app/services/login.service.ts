3f3a1637446a622da84c2a0525ac81fb4709efd7
romelmain <romelherreratgk@gmail.com>
2025-03-26 22:57:17 -0400

    Adding enviroments files

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import Login from '../models/Login';
 import LoginResponse from '../models/LoginResponse';
+import { environment } from '../../enviroments/enviroment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoginService {
-  readonly API_URL = 'http://localhost:8080/auth/log-in';
+  readonly API_URL = environment.loginUrl;
   constructor(private http: HttpClient) {}
 
   postLogin(login: Login) {
