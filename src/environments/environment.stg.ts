38207dcfa0df0750eb7bf646747a77c7a6cb1e42
romelmain <romelherreratgk@gmail.com>
2025-04-19 12:26:42 -0400

    Improvments in stg environment

---
+++ src/environments/environment.stg.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://localhost:3000/api',
+  googleApiKey: 'tu-clave-para-desarrollo',
+  loginUrl: 'https://3.138.85.81:8443/auth/log-in',
+  bookingUrl: 'https://3.138.85.81:8443/booking',
+  menuUrl: 'https://3.138.85.81:8443/menu',
+  roomUrl: 'https://3.138.85.81:8443/rooms',
+  guestUrl: 'https://3.138.85.81:8443/guest',
+};
